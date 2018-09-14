983eeeed0071fdd0c56a49b01980bb2309f18638
Emil Hvid Hansen <EHHN@widex.com>
2018-09-14 10:00:23 +0200

    Add a second test class

---
+++ Githubtest/TestClassTwo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Githubtest
+{
+    public static class TestClassTwo
+    {
+        public static string TestMethod()
+        {
+            return "Callback from test method two";
+        }
+    }
+}
